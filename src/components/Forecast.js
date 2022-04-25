4df292ded02a6fae201ecfe778a0c29e55a066b8
paulinaeriksson <paulina.eriksson@chasacademy.se>
2022-04-25 15:07:17 +0200

    create forecast component

---
+++ src/components/Forecast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Forecast = () => {
+
+      return (
+        <div>
+         <h2>Forecast</h2>
+        </div>
+      );
+    }
+  
+
+  export default Forecast;
