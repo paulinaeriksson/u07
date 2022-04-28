e2826f7a54cb76084722279a5e9cd5876ba9a549
paulinaeriksson <paulina.eriksson@chasacademy.se>
2022-04-28 10:25:51 +0200

    create weather function and display temperature

--- src/App.js
+++ src/App.js
@@ -3,53 +3,102 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 
 //component imports
-import Header from './components/Header';
 import ForecastSevenDays from './components/ForecastSevenDays';
 import ForecastToday from './components/ForecastToday';
 
 
-
 const App = () => {
 
-  /*   const [data, setData] = useState({}); */
-    const [latitude, setLatitude] = useState('');
+/*     const [data, setData] = useState({}); 
+ */ const [latitude, setLatitude] = useState('');
     const [longitude, setLongitude] = useState('');
     const [location, setLocation] = useState('');
-    /* const [unit, setUnit] = useState('metric'); */
+    const [weather, setWeather] = useState({});
+    const [unit, setUnit] = useState('metric');  
+    const [temperature, setTemperature] = useState('');
+
+    const API_key = `8f04b34cdf5219c2aeb1f106bfcd6583`;
+    let API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}&units=metric`;
+
+    const getCity = () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLongitude(position.coords.longitude);
+        setLatitude(position.coords.latitude);
+      
+        })
+          axios.get(API_URL).then((response) => {
+          setLocation(response.data.name);
+        },)
+    }
+    
+
+    const getWeather = async () => {
+      try {
+
+        const response = await axios.get(API_URL);
+        setWeather([response.data]);
+        setTemperature(weather[0].main.temp);
+        console.log(temperature);
+
+      } catch (event) {
+        console.log(event);        
+      }
+    } 
+
 
   
 
-
     useEffect(() => { 
-      navigator.geolocation.getCurrentPosition((position) => {
-      setLongitude(position.coords.longitude);
-      setLatitude(position.coords.latitude);
-      })
-      const API_key = `8f04b34cdf5219c2aeb1f106bfcd6583`;
-      let API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}`;
-      axios.get(API_URL).then((response) => {
-        setLocation(response.data.name);
-      })
+        getCity();
+        getWeather();
+               
+        
+       
+     },[latitude, longitude])
+
+
+
+
+
+
+    //Toggle units
+       const changeUnit = (event) => {
+        event.preventDefault();
+        setUnit(event.target.value);
+        }; 
+
     
-    })
    
     
 
   return (
     <div className="app">
       <div className="container">
+
         <div className="header">
+        <div>
+        <div>
+            <button value="metric" onClick={changeUnit}>C</button>
+        </div>
+        <div>
+            <button value="imperial" onClick={changeUnit}>F</button>
+        </div>
+    </div>
         <h1>{location}</h1>
-      <Header />
-      </div>
-      <h1>Weather</h1>
-      <div className="today"></div>
-      <ForecastToday />
-      </div>
-      <div className="sevendays">
-      <ForecastSevenDays />
+        </div>
+
+        <h2>{temperature}C</h2>
+
+        <div className="today">
+        <ForecastToday />
+        </div>
+
+        <div className="sevendays">
+        <ForecastSevenDays />
+        </div>
       </div>
     </div>
+  
   );
 }
 
