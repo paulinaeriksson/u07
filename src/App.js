ee06c7838d9797d5606721e06153a2e44986448e
paulinaeriksson <paulina.eriksson@chasacademy.se>
2022-05-01 23:18:54 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-//component imports
-import ForecastFiveDays from "./components/ForecastFiveDays";
-import ForecastToday from "./components/ForecastToday";
-
 const App = () => {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
   const [location, setLocation] = useState("");
   const [weather, setWeather] = useState({});
-  const [weatherForecast, setWeatherForecast] = useState({});
-  const [forecastData, setForecastData] = useState({});
   const [unit, setUnit] = useState("metric");
   const [temperature, setTemperature] = useState("");
   const [humidity, setHumidity] = useState("");
@@ -22,22 +16,24 @@ const App = () => {
   const [toggleTemp, setToggleTemp] = useState("C");
   const [toggleWind, setToggleWind] = useState("m/s");
   const [buttonText, setButtonText] = useState("°F");
-  const [weatherImg, setWeatherImg] = useState([{}]);
-  const [date, setDate] = useState("");
   const [daily, setDaily] = useState([]);
   const [hourly, setHourly] = useState([]);
+  const [current, setCurrent] = useState([]);
 
   const API_key = process.env.REACT_APP_API_KEY;
+  //One Call API
   let API_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${API_key}&units=${unit}`;
+  //Current weather API
   let API_URL_1 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}&units=${unit}`;
-  let API_img = `http://openweathermap.org/img/wn/${weatherImg}@2x.png`;
-/*   let API_URL_5 = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_key}&units=${unit}`;
- */
+
+  const sliceDaily = daily.slice(1, 6);
+  const sliceHourly = hourly.slice(0, 24);
+
+  //Get location
   const getCity = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
-      console.log(latitude, longitude);
     });
     if (longitude && latitude) {
       axios.get(API_URL_1).then((response) => {
@@ -46,6 +42,7 @@ const App = () => {
     }
   };
 
+  //Current weather
   const getWeather = async () => {
     try {
       if (longitude && latitude && weather) {
@@ -56,16 +53,7 @@ const App = () => {
         setWindSpeed(response.data.current.wind_speed.toFixed(1));
         setSunset(response.data.current.sunset);
         setSunrise(response.data.current.sunrise);
-        setWeatherImg(response.data.current.weather[0].icon);
-
-        if (response.data) {
-          /*  setTemperature(Math.round(response.data.current.temp));
-         setHumidity(response.data[0].current.humidity);
-          setWindSpeed(response.data.wind.speed.toFixed(1));
-          setSunset(response.data.sys.sunset);
-          setSunrise(response.data.sys.sunrise);
-          setWeatherImg(response.data.weather[0].icon); */
-        }
+        setCurrent(response.data.current.weather[0].icon);
       }
     } catch (event) {
       if (!weather) {
@@ -74,17 +62,13 @@ const App = () => {
     }
   };
 
+  //Hourly and daily forecast
   const getWeatherForecast = async () => {
     try {
       if (longitude && latitude && weather) {
         const res = await axios.get(API_URL);
-        setWeatherForecast([res.data]);
         setDaily(res.data.daily);
         setHourly(res.data.hourly);
-        console.log(res.data.hourly);
-        setDate(res.data.list[0].dt);
-
-      
       }
     } catch (event) {
       if (!weather) {
@@ -96,7 +80,6 @@ const App = () => {
   useEffect(() => {
     getCity();
     getWeather();
-
     getWeatherForecast();
   }, [latitude, longitude, unit]);
 
@@ -116,24 +99,26 @@ const App = () => {
       setUnit("metric");
     }
   };
-/*   console.log(daily[0].temp.day);
- */
-  const sliceDaily = daily.slice(1, 6);
-  const sliceHourly = hourly.slice(0,24);
 
   return (
     <div className="app">
       <div className="flex-container">
         <div className="header"></div>
         <div className="button">
-          <button onClick={changeUnit}>{buttonText}</button>
+          <button onClick={changeUnit}>
+            <strong>{buttonText}</strong>
+          </button>
         </div>
         <section className="today">
           <div>
             <h1>{location}</h1>
           </div>
           <div className="today-img">
-            <img src={API_img} alt="weather icon" />
+            <img
+              className="current-img"
+              src={`http://openweathermap.org/img/wn/${current}.png`}
+              alt={"weather icon"}
+            />
           </div>
           <div className="today-header">
             <h2>
@@ -147,71 +132,109 @@ const App = () => {
           </div>
           <section className="today-section">
             <div className="today-wind">
-              <p>Wind:</p>
+              <p>
+                <strong>Wind:</strong>
+              </p>
               <p>
                 {" "}
                 {windSpeed} {toggleWind}
               </p>
             </div>
             <div className="today-humidity">
-              <p>Humidity:</p>
+              <p>
+                <strong>Humidity:</strong>
+              </p>
               <p>{humidity}%</p>
             </div>
             <div className="today-sunrise">
-              <p>Sunrise:</p>
-              <p>{new Date(sunrise * 1000).toLocaleTimeString("en-GB", {hour:"2-digit", minute: "2-digit"})}</p>
+              <p>
+                <strong>Sunrise:</strong>
+              </p>
+              <p>
+                {new Date(sunrise * 1000).toLocaleTimeString("en-GB", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
             </div>
             <div className="today-sunset">
-              <p>Sunset:</p>
-              <p>{new Date(sunset * 1000).toLocaleTimeString("en-GB", {hour:"2-digit", minute: "2-digit"})}</p>
+              <p>
+                <strong>Sunset:</strong>
+              </p>
+              <p>
+                {new Date(sunset * 1000).toLocaleTimeString("en-GB", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
             </div>
           </section>
         </section>
 
         <section className="fivedays">
-        <h2>Upcoming Weather</h2>
-          {sliceDaily.map((data) => {  
+          <h2>Upcoming Weather</h2>
+          {sliceDaily.map((data) => {
             return (
               <section className="daily-card" key={data.dt}>
-               
-                <h3>{new Date(data.dt * 1000).toLocaleDateString("en-GB", {weekday:"long"})}</h3>
+                <h3>
+                  {new Date(data.dt * 1000).toLocaleDateString("en-GB", {
+                    weekday: "long",
+                  })}
+                </h3>
                 <img
-                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-                alt={data.weather[0].main}
-              />
-                <p><strong>Temperature:</strong> {Math.round(data.temp.day)}°{toggleTemp}</p>
-                <p><strong>Humidity:</strong> {data.humidity}%</p>
-                <p><strong>Wind speed:</strong>{data.wind_speed.toFixed(1)}</p>
+                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                  alt={data.weather[0].main}
+                />
+                <p>
+                  <strong>Temperature:</strong> {Math.round(data.temp.day)}°
+                  {toggleTemp}
+                </p>
+                <p>
+                  <strong>Humidity:</strong> {data.humidity}%
+                </p>
+                <p>
+                  <strong>Wind speed:</strong> {data.wind_speed.toFixed(1)} {toggleWind}
+                </p>
               </section>
             );
           })}
         </section>
 
         <section className="hourly">
-        <h2>Hour by Hour</h2>
-        <div className="card-container">
-        {sliceHourly.map((data) => {  
-            return (
-              
-              <section className="hourly-card" key={data.dt}>
-                <h3>{new Date(data.dt * 1000).toLocaleTimeString("en-GB", {hour: "2-digit", minute: "2-digit" })}</h3>
-                <img
-                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-                alt={data.weather[0].main}
-              />
-                <p><strong>Temperature:</strong> {Math.round(data.temp)}°{toggleTemp}</p>
-                <p><strong>Humidity:</strong></p>
-                <p>{data.humidity}%</p>
-                <p><strong>Wind speed:</strong> {data.wind_speed.toFixed(1)}</p>
-              </section>
-              
-            );
-          })}
-        </div>
+          <h2>Hour by Hour</h2>
+          <div className="card-container">
+            {sliceHourly.map((data) => {
+              return (
+                <section className="hourly-card" key={data.dt}>
+                  <h3>
+                    {new Date(data.dt * 1000).toLocaleTimeString("en-GB", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </h3>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                    alt={data.weather[0].main}
+                  />
+                  <p>
+                    <strong>Temperature:</strong> {Math.round(data.temp)}°
+                    {toggleTemp}
+                  </p>
+                  <p>
+                    <strong>Humidity:</strong>
+                  </p>
+                  <p>{data.humidity}%</p>
+                  <p>
+                    <strong>Wind speed:</strong> {data.wind_speed.toFixed(1)} {toggleWind}
+                  </p>
+                </section>
+              );
+            })}
+          </div>
         </section>
       </div>
     </div>
   );
-}
+};
 
 export default App;
